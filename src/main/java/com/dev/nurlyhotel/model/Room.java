fa6f03a598691539b07ee25c6e3eae6f52e9abe3
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-03-29 19:05:06 +0600

    Создан модель (Entity) для номера

---
+++ src/main/java/com/dev/nurlyhotel/model/Room.java
@@ -0,0 +1,51 @@
+package com.dev.nurlyhotel.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+
+    // Blob – тип данных, который предназначается специально для хранения большого количества электронных материалов, постоянно подвергающихся корректировкам.
+    // BLOB расшифровывается как Binary Large Object. Он используется для хранения массива байт.
+    // Аннотация @Lob указывает, что база данных должна хранить свойство как Large Object.
+    @Lob
+    private Blob photo;
+
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking){
+        if(bookings == null){
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+    }
+}
