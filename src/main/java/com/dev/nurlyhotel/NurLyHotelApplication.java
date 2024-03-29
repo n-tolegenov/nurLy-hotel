6b8d9e0b9ac8be063c424769938f154d3b2ec5fe
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-03-29 16:10:12 +0600

    Initial commit

---
+++ src/main/java/com/dev/nurlyhotel/NurLyHotelApplication.java
@@ -0,0 +1,13 @@
+package com.dev.nurlyhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurLyHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NurLyHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/nurlyhotel/NurLyHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.nurlyhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurLyHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
