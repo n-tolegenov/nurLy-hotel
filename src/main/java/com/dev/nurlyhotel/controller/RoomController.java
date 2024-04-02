307b458bfdcfcaf8728ce4cd532348f3e671de5a
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-04-02 12:12:51 +0600

    Реализован ендпойнт для получения всех типов номера без дубликата - "/rooms/room/types" - Написано собственный скрипт для выборки всех типов номера без дубликата

--- src/main/java/com/dev/nurlyhotel/controller/RoomController.java
+++ src/main/java/com/dev/nurlyhotel/controller/RoomController.java
@@ -5,17 +5,20 @@ import com.dev.nurlyhotel.model.Room;
 import com.dev.nurlyhotel.service.IRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
+/*
+   @RequiredArgsConstructor
+   Generates a constructor with required arguments.
+   Required arguments are final fields and fields with constraints such as @NonNull.
+*/
 @RequiredArgsConstructor
 @RequestMapping("/rooms")
 public class RoomController {
@@ -35,6 +38,11 @@ public class RoomController {
         Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
         RoomDTO roomDTO = new RoomDTO(savedRoom.getId(), savedRoom.getRoomType(), savedRoom.getRoomPrice());
         return ResponseEntity.ok(roomDTO);
+    }
+
+    @GetMapping("/room/types")
+    public List<String> getRoomTypes(){
+        return roomService.getAllRoomTypes();
     }
 
 }
