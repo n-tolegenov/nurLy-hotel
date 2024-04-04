620f4d23c98171da54c098981e26ed76c08f8b20
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-04-04 16:05:06 +0600

    Сервис и репозиторий для управления бронированием номеров: - getAllBookings() - getAllBookingsByRoomId(Long roomId) - cancelBooking(Long bookingId) - saveBooking(Long roomId, BookedRoom bookingRequest) - findByBookingConfirmationCode(String confirmationCode)

--- src/main/java/com/dev/nurlyhotel/service/BookingService.java
+++ src/main/java/com/dev/nurlyhotel/service/BookingService.java
@@ -1,13 +1,78 @@
 package com.dev.nurlyhotel.service;
 
+import com.dev.nurlyhotel.exception.InvalidBookingRequestException;
 import com.dev.nurlyhotel.model.BookedRoom;
+import com.dev.nurlyhotel.model.Room;
+import com.dev.nurlyhotel.repository.BookingRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class BookingService {
-    public List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
-        return null;
+@RequiredArgsConstructor
+public class BookingService implements IBookingService {
+
+    private final BookingRepository bookingRepository;
+    private final IRoomService roomService;
+
+    @Override
+    public List<BookedRoom> getAllBookings() {
+        return bookingRepository.findAll();
     }
+
+
+    public List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
+        return bookingRepository.findByRoomId(roomId);
+    }
+
+    @Override
+    public void cancelBooking(Long bookingId) {
+        bookingRepository.deleteById(bookingId);
+    }
+
+    @Override
+    public String saveBooking(Long roomId, BookedRoom bookingRequest) {
+        if(bookingRequest.getCheckOutDate().isBefore(bookingRequest.getCheckInDate())){
+            throw new InvalidBookingRequestException("Check-in date must come before check-out date");
+        }
+        Room room = roomService.getRoomById(roomId).get();
+        List<BookedRoom> existingBookings = room.getBookings();
+        boolean roomIsAvailable = roomIsAvailable(bookingRequest, existingBookings);
+        if(roomIsAvailable){
+            room.addBooking(bookingRequest);
+            bookingRepository.save(bookingRequest);
+        }else{
+            throw new InvalidBookingRequestException("Sorry, this room is not available for the selected dates");
+        }
+        return bookingRequest.getBookingConfirmationCode();
+    }
+
+    @Override
+    public BookedRoom findByBookingConfirmationCode(String confirmationCode) {
+        return bookingRepository.findByBookingConfirmationCode(confirmationCode);
+    }
+
+    private boolean roomIsAvailable(BookedRoom bookingRequest, List<BookedRoom> existingBookings) {
+        return existingBookings.stream()
+                .noneMatch(existingBooking ->
+                        bookingRequest.getCheckInDate().equals(existingBooking.getCheckInDate())
+                                || bookingRequest.getCheckOutDate().isBefore(existingBooking.getCheckOutDate())
+                                || (bookingRequest.getCheckInDate().isAfter(existingBooking.getCheckInDate())
+                                && bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckOutDate()))
+                                || (bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckInDate())
+
+                                && bookingRequest.getCheckOutDate().equals(existingBooking.getCheckOutDate()))
+                                || (bookingRequest.getCheckInDate().isBefore(existingBooking.getCheckInDate())
+
+                                && bookingRequest.getCheckOutDate().isAfter(existingBooking.getCheckOutDate()))
+
+                                || (bookingRequest.getCheckInDate().equals(existingBooking.getCheckOutDate())
+                                && bookingRequest.getCheckOutDate().equals(existingBooking.getCheckInDate()))
+
+                                || (bookingRequest.getCheckInDate().equals(existingBooking.getCheckOutDate())
+                                && bookingRequest.getCheckOutDate().equals(bookingRequest.getCheckInDate()))
+                );
+    }
+
 }
