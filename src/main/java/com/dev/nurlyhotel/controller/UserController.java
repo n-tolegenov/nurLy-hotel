4e83d07a634c053ddcd9faa16bca39e4f8b27737
n-tolegenov <nurbol.tolegenovv@gmail.com>
2024-04-22 12:04:38 +0600

    Реализован контроллер и сервис для данных пользователя

--- src/main/java/com/dev/nurlyhotel/controller/UserController.java
+++ src/main/java/com/dev/nurlyhotel/controller/UserController.java
@@ -5,6 +5,7 @@ import com.dev.nurlyhotel.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +18,13 @@ public class UserController {
     private final IUserService userService;
 
     @GetMapping("/all")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<User>> getUsers(){
         return new ResponseEntity<>(userService.getUsers(), HttpStatus.FOUND);
     }
 
     @GetMapping("/{email}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> getUserByEmail(@PathVariable("email") String email){
         try {
             User theUser = userService.getUser(email);
@@ -35,6 +38,7 @@ public class UserController {
     }
 
     @DeleteMapping("/delete/{userId}")
+    @PreAuthorize("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN') and #email = principal.username")
     public ResponseEntity<String> deleteUser(@PathVariable("userId") String email){
         try {
             userService.deleteUser(email);
